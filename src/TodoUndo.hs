afef38fef2d3b14f084c20d34db700d3c8fdaf15
pdlla <chippermonky@gmail.com>
2020-04-21 16:18:49 -0700

    clean up trace statements

--- src/TodoUndo.hs
+++ src/TodoUndo.hs
@@ -264,16 +264,16 @@ holdTodo TodoUndoConfig {..} = mdo
   delayed_clear_do_ev <- sequenceEvents (_actionStack_clear as) clear_do_ev
   let
     clearedStackConfig = DynamicStackConfig {
-        _dynamicStackConfig_push = traceEventWith (const "clear1") delayed_clear_do_ev
-      , _dynamicStackConfig_pop = traceEventWith (const "meow1") clear_undo_ev
+        _dynamicStackConfig_push = delayed_clear_do_ev
+      , _dynamicStackConfig_pop = clear_undo_ev
       -- clearing the action stack means we can never "undo" past this point
       -- so clear the cleared stack as well as we don't need the info anymore
       , _dynamicStackConfig_clear = _actionStack_clear as
     }
   clearedStack :: DynamicStack t [(Int, DynTodo t)]
     <- holdDynamicStack [] clearedStackConfig
-  remove_many_ev' :: Event t (Int, DynTodo t) <- repeatEvent $ fmap reindexForRemoval $ traceEventWith (const "clear2") $ _dynamicStack_pushed clearedStack
-  add_many_ev' :: Event t (Int, DynTodo t) <- repeatEvent $ fmap reindexForAddition $ traceEventWith (const "meow2") $ _dynamicStack_popped clearedStack
+  remove_many_ev' :: Event t (Int, DynTodo t) <- repeatEvent $ fmap reindexForRemoval $ _dynamicStack_pushed clearedStack
+  add_many_ev' :: Event t (Int, DynTodo t) <- repeatEvent $ fmap reindexForAddition $ _dynamicStack_popped clearedStack
   let
     remove_many_ev :: Event t (Int, Int)
     remove_many_ev = fmap (\(i,_) -> (i,1)) remove_many_ev'
@@ -285,7 +285,7 @@ holdTodo TodoUndoConfig {..} = mdo
   -- ----------------------
   let
     dsc = DynamicSeqConfig {
-        _dynamicSeqConfig_insert   = leftmost [insert_do_ev, insert_undo_ev, traceEventWith (const "meow") add_many_ev]
+        _dynamicSeqConfig_insert   = leftmost [insert_do_ev, insert_undo_ev, add_many_ev]
         , _dynamicSeqConfig_remove = leftmost [remove_do_ev, remove_undo_ev, remove_many_ev]
         , _dynamicSeqConfig_clear  = never
       }
